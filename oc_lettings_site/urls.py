e7c9c3472ff87d2b6b4af400cdb8477503b19bdf
LiTiBe <contact@litiboost.fr>
2022-07-01 10:53:45 +0200

    update addresses

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 
 from . import views
 
+admin.site.site_header = "Orange Country Lettings"
+admin.site.site_title = "Orange Country Lettings Admin"
+admin.site.index_title = "Welcome to Orange Country Lettings"
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', views.lettings_index, name='lettings_index'),
