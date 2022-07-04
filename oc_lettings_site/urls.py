727640ae179ae961c87387d9c1257db0a8b4227e
LiTiBe <contact@litiboost.fr>
2022-07-04 23:52:36 +0200

    add url sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -7,9 +7,15 @@ admin.site.site_header = "Orange Country Lettings"
 admin.site.site_title = "Orange Country Lettings Admin"
 admin.site.index_title = "Welcome to Orange Country Lettings"
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('', views.index, name='oc_lettings_site_index'),
     path('admin/', admin.site.urls),
     path('lettings/', include("lettings.urls", namespace='lettings')),
     path('profiles/', include("profiles.urls", namespace='profiles')),
+    path('sentry-debug/', trigger_error),
 ]
