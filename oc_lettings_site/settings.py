38d9b9a05bdffc3af2203cbcb869f737efafc1d7
LiTiBe <contact@litiboost.fr>
2022-07-09 15:25:42 +0200

    intégration sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 import dj_database_url
-
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv()
@@ -12,6 +13,23 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0',
                  'oc-lettings-p13-lt.herokuapp.com']
+
+URL_SENTRY = os.environ['SENTRY_SDK']
+sentry_sdk.init(
+    dsn=URL_SENTRY,
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Application definition
 INSTALLED_APPS = [
@@ -28,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
